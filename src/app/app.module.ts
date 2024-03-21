8b9ec3af12896b7c186393e49243c6bb53c0e290
Ivan <ivan@pop-os.localdomain>
2024-03-21 00:38:00 -0300

    criado components de cards, card button e feito tela de planos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CardRoxoComponent } from './card-roxo/card-roxo.component';
+import { CardButtonComponent } from './card-button/card-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardRoxoComponent,
+    CardButtonComponent
   ],
   imports: [
     BrowserModule,
