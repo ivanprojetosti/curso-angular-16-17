1dd0cffa0581477d33450567e3c83f5136c1e0a0
Ivan <ivan@pop-os.localdomain>
2024-03-24 22:00:24 -0300

    criado interpolação para informações de plano e aplicado html dentro do template

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -2,9 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  //templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+
+  template: `
+    <div class="area-plano">
+      <p>{{informacoesCard?.texto}}</p>
+      <p>{{informacoesCard?.preco}}/Mensal - Template</p>
+      <app-card-button></app-card-button>
+    </div>
+  `
 })
 export class CardComponent {
+
+  preco: number = 500;
+  texto: string = "Plano";
+
+  informacoesCard: any = {
+    preco: "R$550,00",
+    texto: "Plano"
+  } 
 
 }
